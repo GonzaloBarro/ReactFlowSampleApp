6b9881a1c6e7593169b4f51b1f9e22a2400c6e09
GonzaloBarro <gonzalo.barro@cloudx.com>
2021-09-22 13:34:11 -0300

    feat(app): add new node button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactFlow from 'react-flow-renderer';
+import React, {useState} from 'react';
+import ReactFlow, {Node, Elements, ElementId} from 'react-flow-renderer';
 
-const elements = [
+const basicElements = [
   { id: "1", data: { label: "Parent" }, position: { x: 500, y: 150 } },
   { id: "2", data: { label: "First child" }, position: { x: 400, y: 250 } },
   { id: "e1-2", source: "1", target: "2", animated: true }
@@ -9,9 +9,25 @@ const elements = [
 
 const graphStyles = { width: "100%", height: "500px" };
 
-const BasicGraph = () => <ReactFlow elements={elements} style={graphStyles} />;
-
 function App() {
+  const [elements, setElements] = useState<Elements>(basicElements);
+
+  const addNode = () => {
+    const nodeId: ElementId = (elements.length + 1).toString();
+    const newNode: Node = {
+      id: nodeId,
+      data: { label: `Node: ${nodeId}` },
+      position: { x: 10, y: 50 },
+    };
+    setElements((els) => els.concat(newNode));
+  };
+
+  const BasicGraph = () => <ReactFlow elements={elements} style={graphStyles}>
+      <button type="button" onClick={addNode} style={{ position: 'absolute', left: 10, top: 10, zIndex: 4 }}>
+        add node
+      </button>
+  </ReactFlow>;
+
   return <BasicGraph />;
 }
 
